b0e4c8fe90acfbd5278e7f59c7ec3473c8e6cdfc
Pritam Makwana <85328035+PritamMakwana@users.noreply.github.com>
2022-06-13 15:05:11 +0530

    class base complete and hide apikey using enviroment var

--- src/components/News.js
+++ src/components/News.js
@@ -6,6 +6,7 @@ import InfiniteScroll from 'react-infinite-scroll-component';
 
 export class News extends Component {
 
+
   static defaultProps = {
     country: 'in',
     pageSize: 8,
@@ -32,19 +33,21 @@ export class News extends Component {
     }
     document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsMonkey`;
   }
-  // 310af44fd67e4b969bb1c55b081266e9
 
   async updateNews() {
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=310af44fd67e4b969bb1c55b081266e9&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.props.setProgress(10);
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
+    this.props.setProgress(30);
     let parsedData = await data.json()
+    this.props.setProgress(70);
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
       loading: false
     })
-
+    this.props.setProgress(100);
   }
   async componentDidMount() {
     this.updateNews();
@@ -62,7 +65,7 @@ export class News extends Component {
 
   fetchMoreData = async () => {
     this.setState({ page: this.state.page + 1 })
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=310af44fd67e4b969bb1c55b081266e9&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json()
     this.setState({
@@ -83,8 +86,6 @@ export class News extends Component {
           hasMore={this.state.articles.length !== this.state.totalResults}
           loader={<Spinner />}
         >
-
-
 
           <div className="container">
             <div className="row">
